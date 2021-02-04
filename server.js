01363b00833086c1ea7af01e0aaa22d44feac840
Emox133 <emir.betaacc0unt@gmail.com>
2021-02-04 19:12:54 +0100

    restored port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(DB, {
     useUnifiedTopology: true
 }).then(() => console.log('DB connection successful'))
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 
 const server = app.listen(port, () => {
     console.log('server started.')
