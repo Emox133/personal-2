dde6bf36c15a592cd0011ddbd5ca2631f74ffe32
Emox133 <emir.betaacc0unt@gmail.com>
2021-02-05 14:47:14 +0100

    error controller fix

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -13,7 +13,9 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500
     err.status = err.status || 'error'
 
-    if(process.env.MODE === 'development') {
-        sendDevErrors(err, res)
-    }
+    // if(process.env.MODE === 'development') {
+    //     sendDevErrors(err, res)
+    // }
+
+    sendDevErrors(err, res)
 }
