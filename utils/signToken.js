b5185d63d92a83fcdb593c26bfe45598083598bb
Emox133 <emir.betaacc0unt@gmail.com>
2021-02-09 03:51:57 +0100

    heroku updated

--- utils/signToken.js
+++ utils/signToken.js
@@ -1,9 +1,4 @@
 const jwt = require('jsonwebtoken')
-const dotenv = require('dotenv')
-
-dotenv.config({
-    path: './config.env'
-})
 
 const signToken = id => {
     return jwt.sign({id}, process.env.JWT_SECRET, {
