2404ce2878fd2a74081ff9b09d3848e514922a0a
Emox133 <emir.betaacc0unt@gmail.com>
2021-03-24 20:17:27 +0100

    errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,5 +1,3 @@
-// const AppError = require('../utils/appError')
-
 const sendDevErrors = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -12,10 +10,6 @@ const sendDevErrors = (err, res) => {
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500
     err.status = err.status || 'error'
-
-    // if(process.env.MODE === 'development') {
-    //     sendDevErrors(err, res)
-    // }
 
     sendDevErrors(err, res)
 }
